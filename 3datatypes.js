8fa601881350790bf0545b1790afa6c62b3bbd0f
pradeep <pradeephs2002@gmail.com>
2025-07-26 17:40:44 +0530

    js datatypes

---
+++ 3datatypes.js
@@ -0,0 +1,28 @@
+//let firstName ="Pradeep" ; //string
+let firstName ="Pradeep"; //string
+
+console.log(`My name is ${firstName} `);
+console.log(typeof firstName);
+
+let num = 10;
+let num2 = 20.2;
+console.log(`${num} , ${num2}`);
+console.log(num,num2);
+
+console.log(typeof true);
+
+var x;
+console.log(typeof x);
+
+console.log(typeof null);
+
+console.log(typeof { });
+console.log(typeof [ ]);
+
+console.log(typeof function(){});
+
+console.log(typeof Symbol());
+
+console.log(typeof BigInt(10));
+
+
